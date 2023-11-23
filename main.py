8844dc3662f9c21decdd15938d24bb6dc9638fee
matvey <matveykakiselev7@gmail.com>
2023-11-23 22:18:27 +0300

    qwe

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram import F
 from aiogram.filters.command import Command
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+import requests
+TOKEN = "6754050049:AAHFbhmkp-ub3xKgYiY6xSq2TT58vQ3XyIs"
+chat_id = "-4050256587"
 privetstviye = "Привет! Это бот для заказа хлебобулочных изделий! Скорее выбирай в меню то, что ты конкретно хочешь."
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -44,11 +47,18 @@ async def stolovaya(message: types.Message):
     await message.answer(text="Выберите позицию из списка.",reply_markup=keyboard)
 @dp.message(F.text.lower() =="сосиска в тесте" or F.text.lower() == "пирожок с капустой" or F.text.lower() == "пирожок с вишней" or F.text.lower() == "пирожок с яблоком" or F.text.lower() == "choco pie"or F.text.lower() == "юбилейное с шоколадом")
 async def zakaz(message: types.Message):
+    prozakaz = message.text
     await message.answer(text="Введите здание, в котором вы находитесь, кабинет, ник в тг.")
     @dp.message(F.text)
     async def zakaz1(message: types.Message):
-        place = message
-        await message.answer(text=place)
+        place = message.text
+        print(place)
+        messageq = f"Место и ник в телеграмм: {place}. Позиция: {prozakaz}"
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={messageq}"
+        print(requests.get(url).json())  # Эта строка отсылает сообщени
+        await message.answer(text="Спасибо за заказ! С вами свяжутся в ближайшее время! Если хотите оформить новый заказ, напишите /start")
+
+
 async def main():
     await dp.start_polling(bot)
 
